2f242d6048008ea0f91d0d23f114612f37a8691b
soucik <soucik@gmail.com>
2018-01-14 16:31:39 +0100

    Added: Drawing width of pulse on display and serial monitor after external interuption

--- main/main.ino
+++ main/main.ino
@@ -1,18 +1,42 @@
 
 #include "U8glib.h"
-int ledPin = 9;
-// setup u8g object, please remove comment from one of the following constructor calls
-// IMPORTANT NOTE: The complete list of supported devices is here: http://code.google.com/p/u8glib/wiki/device
+
+volatile int pwm_value = 0;
+volatile int prev_time = 0;
 U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE);
 
-void draw(void) {
-  // graphic commands to redraw the complete screen should be placed here  
+void draw(int value) {
+  
   u8g.setFont(u8g_font_unifont);
-  u8g.drawStr( 0, 20, "Hello World!");
+  String  val = String(value);
+  u8g.setPrintPos(0, 22);
+  u8g.print("                      ");
+  u8g.setPrintPos(0, 22);
+  u8g.print(val);
 }
 
+void rising() {
+  
+  attachInterrupt(0, falling, FALLING);
+  prev_time = micros();
+}
+ 
+void falling() {
+  
+  pwm_value = micros() - prev_time;
+  Serial.println(pwm_value);
+  u8g.firstPage();  
+  do {
+    draw(pwm_value);
+  } while( u8g.nextPage() );
+  delay(1000);
+  draw(pwm_value);
+  attachInterrupt(0, rising, RISING);
+}
 
 void setup(void) {
+  
+  //  setup display
   u8g.setRot180();
   if ( u8g.getMode() == U8G_MODE_R3G3B2 )
     u8g.setColorIndex(255);     // white
@@ -20,17 +44,17 @@ void setup(void) {
     u8g.setColorIndex(3);         // max intensity
   else if ( u8g.getMode() == U8G_MODE_BW )
     u8g.setColorIndex(1);         // pixel on
+
+    //  setup monitor serial port for pulses length in serial monitor
+    Serial.begin(115200);
+
+    //  set external interrupt on Digital PIN 2
+    attachInterrupt(0, rising, RISING);
 }
+
 void loop(void) {
-  // picture loop
-  u8g.firstPage();  
-  do {
-    draw();
-  } while( u8g.nextPage() );
-  
-  // rebuild the picture after some delay
-  delay(1000);
 }
+
 
 
 

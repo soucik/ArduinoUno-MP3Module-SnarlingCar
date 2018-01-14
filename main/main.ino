0a8baf98874b7941b14e1c32e06a25dbcd060b09
soucik <soucik@gmail.com>
2018-01-14 14:23:03 +0100

    Added: Hello world example

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,36 @@
-void setup() {
 
+#include "U8glib.h"
+int ledPin = 9;
+// setup u8g object, please remove comment from one of the following constructor calls
+// IMPORTANT NOTE: The complete list of supported devices is here: http://code.google.com/p/u8glib/wiki/device
+U8GLIB_SSD1306_128X32 u8g(U8G_I2C_OPT_NONE);
+
+void draw(void) {
+  // graphic commands to redraw the complete screen should be placed here  
+  u8g.setFont(u8g_font_unifont);
+  u8g.drawStr( 0, 20, "Hello World!");
 }
 
-void loop() {
 
+void setup(void) {
+  u8g.setRot180();
+  if ( u8g.getMode() == U8G_MODE_R3G3B2 )
+    u8g.setColorIndex(255);     // white
+  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT )
+    u8g.setColorIndex(3);         // max intensity
+  else if ( u8g.getMode() == U8G_MODE_BW )
+    u8g.setColorIndex(1);         // pixel on
 }
+void loop(void) {
+  // picture loop
+  u8g.firstPage();  
+  do {
+    draw();
+  } while( u8g.nextPage() );
+  
+  // rebuild the picture after some delay
+  delay(1000);
+}
+
+
+
